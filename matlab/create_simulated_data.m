874a6ba0ed780789bfc99366dedb7e8446a00d83
plhrja <robert.sirvio@helsinki.fi>
2014-04-10 18:41:32 +0300

    Cleaned the output of plot_and_save and harvested some data

--- matlab/create_simulated_data.m
+++ matlab/create_simulated_data.m
@@ -12,7 +12,7 @@ show_plot = 0;
 % which filter should be used?
 % 0: female
 % 1: male
-data_male_filter = 0;
+data_male_filter = 1;
 
 % which glottal model?
 % 0: triangle wave
@@ -20,8 +20,8 @@ data_male_filter = 0;
 klatt_model = 1;
 
 % parameters for the data
-f = 160;
-Q = .7;
+f = 140;
+Q = .6;
 Q_rand = 0;
 noise_lvl = .005;
 periods = 10;
